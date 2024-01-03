5a28544f8bb6a978b575eb472241e9b9891ce7b3
Anastasija <anastasijakoles@gmail.com>
2024-01-03 19:00:52 +0300

    feat: add HW8 with styles

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -19,15 +19,16 @@ export type UserType = {
 
 const initialPeople: UserType[] = [
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+    {_id: 0, name: 'Kate', age: 3},
+    {_id: 1, name: 'Alex', age: 66},
+    {_id: 2, name: 'Nick', age: 16},
+    {_id: 3, name: 'Victor', age: 44},
+    {_id: 4, name: 'David', age: 40},
+    {_id: 5, name: 'Ingrid', age: 55},
 ]
 
 const HW8 = () => {
+
     const [people, setPeople] = useState<UserType[]>(initialPeople)
     const [currentSort, setCurrentSort] = useState('')
 
