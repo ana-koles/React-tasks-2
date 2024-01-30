924fc103d0a7537f6ea021ef95ff38a5d8f90e4c
Anastasija <anastasijakoles@gmail.com>
2024-01-30 16:53:37 +0300

    feat: add sliders to Junior Plus page

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,17 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                "& .MuiSlider-thumb": {
+                    backgroundColor: '#00CC22', // Цвет для ползунка слайдера
+                },
+                "& .MuiSlider-track": {
+                    backgroundColor: '#00CC22', // Цвет для невыбранного диапазона
+                },
+                "& .MuiSlider-rail": {
+                    backgroundColor: 'grey', // Цвет для выбранного диапазона
+                },
+                width: '147px',
+                height: '4px'
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
